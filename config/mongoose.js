70705df133f20cc6c8a58173c88cd0544623cc64
Harshita Sajjan <harshitasajjan07@gmail.com>
2024-03-03 23:00:49 +0530

    Passport

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+/****
+ * const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/SocioSphere_Development');
 
@@ -7,6 +8,23 @@ db.on('error', console.error.bind(console, 'Error conntect to Database'));
 
 db.once('open', function(){
     console.log('Succesfully connected to Database');
+});
+
+module.exports = db;
+*/
+
+const mongoose = require('mongoose');
+
+const mongoUrl = 'mongodb://localhost/SocioSphere_Development';
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+
+db.once('open', function () {
+    console.log('Connected to database :: MongoDB');
 });
 
 module.exports = db;
